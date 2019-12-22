ce3d61dfb6c68784f1297cd9bd417a3ac2a98764
MohamedHmini <mohamedhmini1997@gmail.com>
2019-12-22 22:43:49 +0100

    adding some semantic validation functions (processors) and

--- lion.py
+++ lion.py
@@ -1,7 +1,6 @@
-from textx import metamodel_from_file
+from textx import metamodel_from_file, model, metamodel
 from processors import p1
-
-
+# from generator import genCode
 
 
 mm = metamodel_from_file('LION_META_MODEL.tx')
@@ -9,8 +8,10 @@ mm = metamodel_from_file('LION_META_MODEL.tx')
 # inject the processor into the meta-model : 
 p1.inject_processor(mm)
 
-
 m = mm.model_from_file('instance.lion')
+# print(model.get_children_of_type('LoadRule', m))
+# generated_file_path = getCode(m)
 
-
-print(m.rules[0].file_path)
+# print(dir(mm.rootcls._tx_attrs['rules'].cls))
+# print(dir(metamodel))
+# print(dir(mm.namespaces['LION_META_MODEL']))
--- processors/p1.py
+++ processors/p1.py
@@ -1,39 +1,47 @@
 from textx.exceptions import *
+from textx import model
 
 
-def general_obj_processor(general):
-
-    lloads = len(general.rules)
-
-    
+def lionframe_unique_declaration(loads, err):
+    lloads = len(loads)
 
     for i in range(lloads-1):
         for j in range(i+1, lloads):
-            if general.rules[i].lionFrame == general.rules[j].lionFrame:
-                raise TextXSemanticError('lionFrame variable name ("%s") is already used!' % general.loadrules[i].lionFrame)
+            if loads[i].lionFrame == loads[j].lionFrame:
+                raise TextXSemanticError(err % loads[i].lionFrame)
+    
 
-    pass
+def column_unique_declaration(cols, err):
+    lcols = len(cols)
 
+    for i in range(lcols-1):
+        for j in range(i+1, lcols):
+            if cols[i].v_name == cols[j].v_name:
+                raise TextXSemanticError(err % cols[i].v_name)
+
+
+
+# OBJ PROCESSORS : 
+
+def main_obj_processor(main_rule):
+    loads = model.get_children_of_type('LoadRule', main_rule) 
+    lionframe_unique_declaration(loads,'lionFrame variable name ("%s") is already used!')    
+    
 
 def load_obj_processor(load):
-    # l = len(load.columns)
-
-    # for i in range(l-1):
-    #     for j in range(i+1, l):
-    #         if general.loadrules[i].lionFrame == general.loadrules[j].lionFrame:
-    #             raise TextXSemanticError('lionFrame variable name ("%s") is already used!' % general.loadrules[i].lionFrame)
-
-    pass
-
-
+    column_unique_declaration(load.columns,'column name ("%s") is already used in ("{}")'.format(load.lionFrame)) 
+    
+# def column_obj_processor(column):
+#     if column._tx_attrs['type'].cls.__name__ != "Dtype":
+#         raise TextXSemanticError("data type {} doesn't exist!".format(column.type))
+    
 
 
 def inject_processor(mm):
     obj_processors = {
-        'GeneralRule': general_obj_processor
-        # 'LoadRule': load_obj_processor
+        'MainRule': main_obj_processor,
+        'LoadRule': load_obj_processor
+        # 'ColumnRule':column_obj_processor
     }
 
     mm.register_obj_processors(obj_processors)
-
-    pass
